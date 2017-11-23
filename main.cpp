0a815e316f7e636129f977ebde20159de181686e
arnlaugs <arnlaugskj@gmail.com>
2017-11-23 14:55:28 +0100

    lagt til lambda

--- main.cpp
+++ main.cpp
@@ -11,14 +11,15 @@ std::random_device rd;
 std::mt19937_64 gen(rd());
 std::uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
 
-void Transaction(double *values, int agent_1, int agent_2);
+void Transaction(double *values, int agent_1, int agent_2, double lambda);
 
 int main()
 {
-    int N = 50;
+    int N = 500;
     int initial_money = 1; //Expressed in multiples of M_0
     double bank[N];
     double total_bank[N];
+    double lambda=0;
     for(int agent = 0; agent < N; agent++){
         bank[agent] = initial_money;
         total_bank[agent] = 0;
@@ -26,8 +27,8 @@ int main()
 
     int agent_1 = 0, agent_2 = 1;
 
-    int total_runs = pow(10,3);
-    int total_time = pow(10,6);
+    int total_runs = pow(10,0);
+    int total_time = pow(10,7);
 
     int money_steps_per = 100; // Per unit aka d_money = 0.1
     int maximum_money = initial_money*10; // Guessed value, to inlcude most of the measurements.
@@ -40,7 +41,7 @@ int main()
         histogram[index] = 0;
     }
 
-    ofile.open("Histogram time=6, runs=3, N=50, dm=0.01.txt");
+    ofile.open("Histogram time=7, runs=0, N=500, dm=0.01.txt");
 
     for(int current_run = 0; current_run < total_runs; current_run++){
 
@@ -57,7 +58,7 @@ int main()
                     agent_2 = RandomNumberGenerator(gen)*N;
                 }
 
-                Transaction(bank, agent_1, agent_2);
+                Transaction(bank, agent_1, agent_2, lambda);
         }
 
         cout << "\n Run " << (current_run+1) << " finished.\n";
@@ -90,10 +91,10 @@ int main()
 
 
 
-void Transaction(double *values, int agent_1, int agent_2){
+void Transaction(double *values, int agent_1, int agent_2, double lambda){
 
     double epsilon = RandomNumberGenerator(gen);
     double sum = values[agent_1] + values[agent_2];
-    values[agent_1] = epsilon*sum;
-    values[agent_2] = (1-epsilon)*sum;
+    values[agent_1] = lambda*values[agent_1]+ epsilon*(1-lambda)*sum;
+    values[agent_2] = lambda*values[agent_2]+ (1-epsilon)*(1-lambda)*sum;
 }
